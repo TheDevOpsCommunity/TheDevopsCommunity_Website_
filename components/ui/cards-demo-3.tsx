364608d8a5a4640d68a58e9dafa2c110e26d9de8
pra2107tham <prathamshirbhate1909@gmail.com>
2025-05-17 16:41:29 +0530

    Refactor CardSkeletonContainer styles by removing unnecessary height adjustment for improved layout consistency.

--- components/ui/cards-demo-3.tsx
+++ components/ui/cards-demo-3.tsx
@@ -238,7 +238,7 @@ export const CardSkeletonContainer = ({
   return (
     <div
       className={cn(
-        "h-[15rem] md:h-[20rem] rounded-xl z-40",
+        "h-[15rem] rounded-xl z-40",
         className,
         showGradient &&
           "bg-purple-200 [mask-image:radial-gradient(50%_50%_at_50%_50%,white_0%,transparent_100%)]"
